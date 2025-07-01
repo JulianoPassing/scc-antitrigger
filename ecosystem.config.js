2462462148c5443eee679a4dafbcf4a7df22feb8
JulianoPassing <julianosalm@gmail.com>
2025-06-30 23:23:26 -0300

    att
    att

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,38 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: 'scc-antitrigger-bot-1',
+      name: 'scc-antitrigger-bot',
       script: 'bot.py',
       interpreter: './venv/bin/python',
       cwd: '/root/Desktop/scc-antitrigger',
       env: {
-        NODE_ENV: 'production',
-        INSTANCE: '1'
+        NODE_ENV: 'production'
       },
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       log_file: './logs/combined.log',
-      out_file: './logs/out-1.log',
-      error_file: './logs/error-1.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
-    },
-    {
-      name: 'scc-antitrigger-bot-2',
-      script: 'bot.py',
-      interpreter: './venv/bin/python',
-      cwd: '/root/Desktop/scc-antitrigger',
-      env: {
-        NODE_ENV: 'production',
-        INSTANCE: '2'
-      },
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      log_file: './logs/combined.log',
-      out_file: './logs/out-2.log',
-      error_file: './logs/error-2.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      instances: 1,
+      exec_mode: 'fork'
     }
   ]
 } 
