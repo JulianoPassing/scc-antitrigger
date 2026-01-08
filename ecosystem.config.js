b61ab7be213460c71e528bab6fbfac18c76102d5
JulianoPassing <julianosalm@gmail.com>
2026-01-08 14:03:39 -0300

    1403
    1403

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,14 @@
 module.exports = {
-  apps: [
-    {
-      name: 'scc-antitrigger',
-      script: 'bot.py',
-      interpreter: '/usr/bin/python3.11',
-      cwd: '/home/juliano/Desktop/scc-antitrigger',
-      env: {
-        NODE_ENV: 'production'
-      },
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      log_file: './logs/combined.log',
-      out_file: './logs/out.log',
-      error_file: './logs/error.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      instances: 1,
-      exec_mode: 'fork'
+  apps: [{
+    name: 'scc-antitrigger',
+    script: 'bot.py',
+    interpreter: 'python',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
     }
-  ]
-} 
+  }]
+};
